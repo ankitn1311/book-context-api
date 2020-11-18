d0d54d3cdc912fdd6e9a98de9d26be53a75343a1
Ankit Negi <ankitn1311@gmail.com>
2020-11-19 01:30:45 +0530

    fixed issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,19 @@ import { BookContext } from '../contexts/BookContext';
 
 const Navbar = () => {
     const { books } = useContext(BookContext);
-    return (
+    return books.length > 0 ? (
         <nav>
             <h1>My Book App</h1>
             <h5>There are {books.length} books</h5>
         </nav>
-    )
+    ) : (
+            <nav>
+                <h1>My Book App</h1>
+                <div>
+                    <p style={{ textAlign: 'center' }}>There are books to read...just enjoy :)</p>
+                </div>
+            </nav>
+        )
 }
 
 export default Navbar;
