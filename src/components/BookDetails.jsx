3dd31e776e1fa470fe01bedf684a7fd7b2b4b90a
Ankit Negi <ankitn1311@gmail.com>
2020-11-19 01:23:25 +0530

    delete feature added

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { BookContext } from '../contexts/BookContext';
 
 function BookDetails({ books }) {
+    const { deleteBook } = useContext(BookContext);
     return (
         <div>
             {books.map(book => {
                 return (
-                    <li key={book.id}>
+                    <li key={book.id}
+                        onClick={() => deleteBook(book.id)}>
                         <div className="title">{book.title}</div>
                         <div className="author">{book.author}</div>
                     </li>
